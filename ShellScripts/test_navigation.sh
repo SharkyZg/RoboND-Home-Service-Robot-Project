7a6a83ab5e77f53e2fff668d26c6a37c42bbb937
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-17 13:39:49 +0200

    test_navigation.sh created

--- ShellScripts/test_navigation.sh
+++ ShellScripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm  -e  " export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/gazebo.world && source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/gazebo2.world" &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/World/map.yaml && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
+xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/World/map.pgm && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " 
