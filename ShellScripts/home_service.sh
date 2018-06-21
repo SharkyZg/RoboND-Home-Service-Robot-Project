34ae7e420ff2aaa303d3d592837a23b03ba00e32
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-21 14:22:33 +0200

    add_markers.cpp wip

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -5,5 +5,5 @@ xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE=/
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
 sleep 10
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../add_markers/launch/add_markers.launch " &&
+xterm  -e  " source ../../devel/setup.bash && roslaunch ../add_markers/launch/add_markers.launch " &
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../pick_objects/launch/pick_objects.launch "
