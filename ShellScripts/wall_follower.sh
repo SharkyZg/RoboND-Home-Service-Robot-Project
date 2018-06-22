853a2b4dd2346be12d3d52b8fed5bad604b355c7
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-22 08:06:26 +0200

    paths corrected

--- ShellScripts/wall_follower.sh
+++ ShellScripts/wall_follower.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/gazebo.world && source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/gazebo2.world" &
+xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch" &
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/gmapping_demo.launch" & 
 sleep 5
