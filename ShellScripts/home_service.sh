3ffae6f8015559875a76a2136518f46aaa43897d
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-22 07:35:00 +0200

    paths corrected

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " export TURTLEBOT_GAZEBO_WORLD_FILE=../World/gazebo.world && source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=../World/gazebo2.world" &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE=../World/map.yaml && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
+xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE=$(find turtlebot_simulator)/World/map.yaml && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
 sleep 10
