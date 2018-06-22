7ed23ab2587f5a82a2035ff7c686659b09770c08
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-22 07:56:58 +0200

    paths corrected

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch" &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && export TURTLEBOT_GAZEBO_MAP_FILE= && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
+xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
 sleep 10
