062d3fd4c1e006fcd601231063d5cd3c252a0144
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-22 11:02:52 +0200

    -

--- ShellScripts/home_service.sh
+++ ShellScripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch" &
+xterm  -e  " source ~/catkin_ws/devel/setup.bash && roslaunch ~/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=~/catkin_ws/src/World/gazebo.world" &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
+xterm  -e  " source ~/catkin_ws/devel/setup.bash && roslaunch ~/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch map_file:=~/catkin_ws/src/World/map.yaml" & 
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
+xterm  -e  " source ~/catkin_ws/devel/setup.bash && roslaunch ~/catkin_ws/src/turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
 sleep 10
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../add_markers/launch/add_markers.launch " &
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../pick_objects/launch/pick_objects.launch "
+xterm  -e  " source ~/catkin_ws/devel/setup.bash && roslaunch ~/catkin_ws/src/add_markers/launch/add_markers.launch " &
+xterm  -e  " source ~/catkin_ws/devel/setup.bash && roslaunch ~/catkin_ws/src/pick_objects/launch/pick_objects.launch "
--- ShellScripts/test_navigation.sh
+++ ShellScripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch" &
+xterm  -e  " export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/gazebo.world && source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/gazebo2.world" &
 sleep 5
-xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch" & 
+xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_simulator/turtlebot_gazebo/launch/amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/map.yaml" & 
 sleep 5
 xterm  -e  " source ../../devel/setup.bash && roslaunch ../turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " 
